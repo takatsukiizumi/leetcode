38b9886b646b4cedc7ec50e399f8124edee4868f
takatsukiizumi <wildartist@foxmail.com>
2023-03-20 11:22:25 +0800

    test

--- src/main/java/redis/Theory/RedissionTheory.java
+++ src/main/java/redis/Theory/RedissionTheory.java
@@ -31,7 +31,7 @@ public class RedissionTheory {
     //RCountDownLatch - Redis的分布式计数器实现。
     //RTopic - Redis的发布/订阅实现，支持分布式。
     //RLiveObject - Redis的对象存储实现，支持JPA和POJO。
-    //branch1
-    //branch2
-    //branch3
+    //master1
+    //master2
+    //master3
 }
